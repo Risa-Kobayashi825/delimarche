dd3fdce32c473720bbf20ff216e5f55a07fb24ed
risakobayashi <risap1275@gmail.com>
2021-08-24 23:03:59 +0000

    user side

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -60,7 +60,7 @@ class ProductsController < ApplicationController
    end
    
    def product_params
-      params.require(:product).permit(:name, :description, :price, :category_id)
+      params.require(:product).permit(:name, :description, :price, :category_id, :shop_name)
    end
 
    def sort_params
